d33191dc414e0766ae3bf7d446f18c1cc63d26e8
omarturmun <93262369+omarturmun@users.noreply.github.com>
2022-01-28 13:54:21 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,93 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        eskuineko_intermitentea()
+    }
+    if (receivedNumber == 2) {
+        eskerreko_intermitentea()
+    }
+    if (receivedNumber == 3) {
+        limpiaparabrisas()
+    }
+})
+function limpiaparabrisas () {
+    pins.servoWritePin(AnalogPin.P0, 180)
+}
+function eskerreko_intermitentea () {
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+function eskuineko_intermitentea () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+radio.setGroup(6)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    }
+    // 
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    }
+    if (input.logoIsPressed()) {
+        radio.sendNumber(3)
+    }
+})
